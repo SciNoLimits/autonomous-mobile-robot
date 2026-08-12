161ff6200930a2d87b4c160f0ee2dbf303ae1538
SciNoLimits <prazwaldutta7@gmail.com>
2026-08-12 21:47:50 +0200

    Add launch file for AMR navigation to include controller and patrol launches

---
+++ amr_navigation/launch/amr.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Get the path to the amr_controller and amr_navigation package
+    amr_controller_share_dir = get_package_share_directory('amr_controller')
+    amr_navigation_share_dir = get_package_share_directory('amr_navigation')
+
+    # Create an IncludeLaunchDescription action for the controller.launch.py file
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(amr_controller_share_dir, 'launch', 'controller.launch.py')
+        )
+    )
+    
+    patrol_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(amr_navigation_share_dir, 'launch', 'patrol.launch.py')
+        )
+    )
+
+    # Create and return the LaunchDescription
+    ld = LaunchDescription()
+    ld.add_action(controller_launch)
+    ld.add_action(patrol_launch)
+    return ld
